c3b60e193c1825903f06f005a239d5abf7230d0c
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-09-15 23:41:05 +0100

    Add data to the columns

--- data.sql
+++ data.sql
@@ -11,3 +11,10 @@ INSERT INTO animals VALUES(8,'Angemon','2005-06-12',1,true,-45);
 INSERT INTO animals VALUES(9,'Boarmon','2005-06-07',7,true,20.4);
 INSERT INTO animals VALUES(10,'Blossom','1998-10-13',3,true,17);
 INSERT INTO animals VALUES(11,'Ditto','2022-05-14',4,true,22);
+
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34),
+ ('Jennifer Orwell', 19),
+ ('Bob',45), ('Melody Pond', 77), 
+ ('Dean Winchester', 14), 
+ ('Joddie Whittaker', 38);
